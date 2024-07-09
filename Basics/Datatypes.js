da8a545192ed2032d2b82d38cf304ae53116015e
Mayank Tyagi <129610933+SpectaclesGuy@users.noreply.github.com>
2024-07-09 08:41:35 +0000

    About datatypes

---
+++ Basics/Datatypes.js
@@ -0,0 +1,26 @@
+"use script"; //treat all JS code as newer versions
+
+//JavaScript is a dynamic language with dynamic types. 
+// Variables in JavaScript are not directly associated with any particular value type, 
+// and any variable can be assigned (and re-assigned) values of all types:
+
+// let abc = 42; abc is now a number
+// abc = "bar"; abc is now a string
+// abc = true; abc is now a boolean
+
+// Primitive data types
+// number -> (-2^53 - 1) to (2^53 - 1)
+// bigint
+// string -> ""
+// boolean -> true or false
+// null -> standalone value
+// undefined -> value has not been assigned yet
+// symbol -> for uniqueness  
+
+
+
+// Objects
+
+// typeof() can be used to know thw type of something
+// typeof(null) -> object
+// typeof(undefined) -> undefined
