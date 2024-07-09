22a5accd6c5772ea6add742a3a368ad305930392
Mayank Tyagi <129610933+SpectaclesGuy@users.noreply.github.com>
2024-07-09 17:42:45 +0000

    added datatype summary

---
+++ Basics/05_datatypeSummary.js
@@ -0,0 +1,19 @@
+// Datatypes are basically divided into two:
+// Primivite and Non-Primitive(Reference)
+// This division is based in the way that how they are stored in the memory
+
+// Primitive :
+//      7 types : String, Number, Boolean, Bigint, null, undefined, symbol
+
+// Reference (Non-Primitive):
+//      3 types: Array, Objects, Functions
+
+// Javascript is dynamically typed as when declaring a variable we never mention its datatype
+
+// In Javascript, the datatype Number includes both float and integer values
+
+// the data type of all reference datatypes returned by the typeof() function is object, expect for a function,
+// in which case it is an object function
+
+
+// All the datatypes that have not yet discussed will be added earlier
